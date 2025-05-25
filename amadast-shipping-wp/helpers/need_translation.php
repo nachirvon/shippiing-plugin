cd5416f056a4b7c2e1f11d581eb4d58a9dad5cb6
nachirvon <nachirvon@gmail.com>
2025-05-25 10:32:52 +0330

    Delete amadast-shipping-wp directory

--- amadast-shipping-wp/helpers/need_translation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!defined('ABSPATH')) exit;
-
-if (!function_exists('amdsp_need_translation')) {
-    function amdsp_need_translation(): bool {
-        return get_locale() !== 'fa_IR';
-    }
-}
